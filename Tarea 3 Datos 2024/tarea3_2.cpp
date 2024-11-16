2b815b5a4dff5ce7d24e513ac0ac55d7d3adb370
SMR0411 <145221247+SMR0411@users.noreply.github.com>
2024-11-16 17:58:05 -0300

    Add files via upload

--- Tarea 3 Datos 2024/tarea3_2.cpp
+++ Tarea 3 Datos 2024/tarea3_2.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 using namespace std;
 
 struct Plato {
-    std :: string nombre ;
+    std::string nombre ;
     int precio ;
 };
 
@@ -14,10 +15,27 @@ class Pedido {
     size_t cant_platos ;
 
     public :
-    Pedido () ;
-    ~ Pedido () ;
-    void agregar_plato ( Plato * plato ) ; // agrega un plato al pedido
-    int precio_total () ; // retorna la suma del precio de todos los platos del pedido
+    Pedido (bool servir) : platos(new Plato[25]), servir(servir), cant_platos(0) {
+
+    };
+    ~ Pedido (){
+        delete[] platos;
+    } ;
+    void agregar_plato ( Plato* plato ){
+        platos[cant_platos].nombre = plato->nombre;
+        platos[cant_platos].precio = plato->precio;
+        cant_platos += 1;
+        cout<<platos[cant_platos-1].nombre<<endl;
+    } ; // agrega un plato al pedido
+    int precio_total (){
+        int n= 0;
+        int precio_d= 0;
+        while(platos[n].precio != 0){
+            precio_d += platos[n].precio;
+            n+=1;
+        };
+        return precio_d;
+    } ; // retorna la suma del precio de todos los platos del pedido
 };
 
 class Registro {
@@ -44,15 +62,15 @@ int main(){
         return 1;
     }
 
-    int cant_platos , *precio;
-    string line , pa , *plato , *l_m;
+    int cant_platos;
+    string line , pa, pe, *l_m;
     getline(file, line);
     cant_platos = stoi(line);
 
     cout<<cant_platos<<endl;
 
-    precio = new int[cant_platos];
-    plato = new string[cant_platos];
+    Plato* Orden;
+    Orden = new Plato[cant_platos];
     l_m = new string[cant_platos];
 
     for(int i = 0; i<cant_platos; i++){
@@ -66,25 +84,31 @@ int main(){
     for(int j = 0; j<cant_platos; j++){
         int posac = 0;
         pa = "";
+        pe = ""; 
         for(l_m[j][posac]; l_m[j][posac] != '-' ; posac++){
-            plato[j] += l_m[j][posac]; 
+            pe += l_m[j][posac]; 
         }
-
-        cout<<plato[j]<<endl;
+        Orden[j].nombre = pe;
+        cout<<Orden[j].nombre<<endl;
 
         posac +=1;
         for(l_m[j][posac]; l_m[j][posac] != ' '; posac++){
             pa += l_m[j][posac];
         }
-        precio[j] = stof(pa);
+        Orden[j].precio = stof(pa);
 
-        cout<<precio[j]<<endl;
+        cout<<Orden[j].precio<<endl;
     }
 
+    Pedido* prueba= new Pedido(true);
+    prueba->agregar_plato(&Orden[0]);
+    prueba->agregar_plato(&Orden[4]);
+    cout<<prueba->precio_total()<<endl;
 
 
-    delete[] precio;
-    delete[] plato;
+
+
+    delete[] Orden;
     delete[] l_m;
     file.close();
     return 0;
