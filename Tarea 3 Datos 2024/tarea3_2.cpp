461a2a1286dca1d03a2cd8089c0063898adcfeb7
SMR0411 <145221247+SMR0411@users.noreply.github.com>
2024-11-24 16:05:26 -0300

    Add files via upload

--- Tarea 3 Datos 2024/tarea3_2.cpp
+++ Tarea 3 Datos 2024/tarea3_2.cpp
@@ -194,10 +194,11 @@ int main() {
         return 1;
     }
 
-    int cant_platos;
+    int cant_platos, plat_act;
     string line, pa, pe, *l_m;
     getline(file, line);
     cant_platos = stoi(line);
+    plat_act= cant_platos;
 
     Plato *Orden;
     Orden = new Plato[cant_platos];
@@ -243,6 +244,76 @@ int main() {
     testing->get_pedido(4, true);
     cout << "Factor de carga: " << testing->dar_fc() << endl;
     testing->eliminar_pedido(4, true);
+    
+    Registro *Main;
+    bool flag = true;
+    bool primera_vez= true;
+    while(flag){
+        string input , acción , exacta, mas_exacta, exacta_res;
+        getline(cin,input);
+        if(primera_vez==true){
+            Main->mesas_empezar(stoi(input));
+            primera_vez= false;
+        }
+
+        if(input == "cerrar"){
+            break;
+        }
+
+        size_t espacio_pos = input.find(' ');
+        acción = input.substr(0,espacio_pos);
+        exacta = input.substr(espacio_pos + 1);
+        size_t espacio_pos2= exacta.find(' ');
+        mas_exacta= exacta.substr(espacio_pos2+1);
+        exacta_res= exacta.substr(0,espacio_pos2);
+
+
+        if(acción == "registrar"){
+            bool sol;
+            if(exacta_res=="mesa"){
+                sol= true;
+            }
+            if(exacta_res=="llevar"){
+                sol= false;
+            }
+            Pedido *temporal= new Pedido();
+            temporal->definir_TFyID(sol, stoi(mas_exacta));
+            bool flag_2= true;
+            while(flag_2){
+                string frase, orden, plato;
+                getline(cin,frase);
+
+                if(frase=="pedir"){
+                    Main->agregar_pedido(temporal);
+                    cout<<exacta+" Registrado"<<endl;
+                    delete[] temporal;
+                    break;
+                }
+                size_t pos_act = frase.find(' ');
+                orden = frase.substr(0,pos_act);
+                plato = frase.substr(pos_act + 1);
+                if(orden=="agregar"){
+                    for(int p= 0; p<plat_act; p++){
+                        if(Orden[p].nombre==plato){
+                            temporal->agregar_plato(&Orden[p]);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(acción == "sm"){
+        
+        }
+
+        if(acción == "br"){
+        
+        }
+
+        if(acción == "wr"){
+        
+        }
+    }
 
 
     delete[] Orden;
